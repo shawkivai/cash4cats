eb704ebd18c723200dfc2acbfe02dbba39510de0
Mustakim <mustakim@energaia.com>
2019-10-13 21:51:14 +0600

    base url fix

--- public_html/application/config/config.php
+++ public_html/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://cash4cats.test/";
+$config['base_url'] = "http://13.210.135.225/public_html/";
 
 /*
 |--------------------------------------------------------------------------
