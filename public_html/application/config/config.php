3a2f30fd7ee622cc929c6b8e3c1a142b3113cf5c
Mustakim <mustakim@energaia.com>
2020-02-01 03:33:41 +0600

    Fix: Temporary fixed GST in override price

--- public_html/application/config/config.php
+++ public_html/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://0415236291.space";
+$config['base_url'] = "http://13.210.135.225/";
 
 /*
 |--------------------------------------------------------------------------
